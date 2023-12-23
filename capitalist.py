ac09f513a81125452f577435f643e8fefc389845
= <=>
2023-12-23 18:35:26 +0000

    Make __repr__ format consistent by removing errant full stop

--- capitalist.py
+++ capitalist.py
@@ -4,7 +4,7 @@ class Stock:
     self.holdings = 0.0
     self.fees = 10
   def __repr__(self):
-    return "This stock has {} price, {} holdings and {} fees.".format(self.price, self.holdings, self.fees)
+    return "This stock has {} price, {} holdings and {} fees".format(self.price, self.holdings, self.fees)
   def buy(self, amount, bank):
     self.holdings += (amount - self.fees) / self.price
     band.withdraw(amount)
