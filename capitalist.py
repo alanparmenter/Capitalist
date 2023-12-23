f11a3662abc2f109c1d50781c93bb7565fddbd9e
= <=>
2023-12-23 18:03:39 +0000

    Add buy, sell and get_balance methods to Stock class in capitalist.py

--- capitalist.py
+++ capitalist.py
@@ -5,3 +5,11 @@ class Stock:
     self.fees = 10
   def __repr__(self):
     return "This stock has {} price, {} holdings and {} fees.".format(self.price, self.holdings, self.fees)
+  def buy(self, amount, bank):
+    self.holdings += (amount - self.fees) / self.price
+    band.withdraw(amount)
+  def sell(self, amount, bank):
+    self.holdings -= amount / self.price
+    bank.deposit(amount - self.fees)
+  def get_balance(self):
+    return self.holdings * self.price
