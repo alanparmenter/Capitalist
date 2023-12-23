9d99cbcc9a97e13ebe1cdd36ac6b5c211f984262
= <=>
2023-12-23 18:14:19 +0000

    Add Bank class with attributes and string representation

--- capitalist.py
+++ capitalist.py
@@ -13,3 +13,11 @@ class Stock:
     bank.deposit(amount - self.fees)
   def get_balance(self):
     return self.holdings * self.price
+
+class Bank:
+  def __init__(self, initial_deposit):
+    self.name = "Anonymous Bank"
+    self.cash = initial_deposit
+    self.interest_rate = 0
+  def __repr__(self):
+    return "This bank named {} has {} cash and an interest rate of {}".format(self.name, self.cash, self.interest_rate)_
