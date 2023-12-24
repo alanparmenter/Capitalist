46985128419fd2d0118947ce70507371755ef0c8
= <=>
2023-12-24 07:43:26 +0000

    Add print statements from method calls as tests

--- capitalist.py
+++ capitalist.py
@@ -1,2 +1,12 @@
 from bank import Bank
 from stock import Stock
+
+feta = Stock(300)
+farclays = Bank(10000)
+print("Farclays initial balance: " + str(farclays.get_balance()))
+feta.buy(310, farclays)
+print("Feta holdings as balance after purchase: " + str(feta.get_balance()))
+print("Farclays balance after purchase: " + str(farclays.get_balance()))
+feta.sell(300, farclays)
+print("Feta holdings as balance after sale: " + str(feta.get_balance()))
+print("Farclays balance after purchase: " + str(farclays.get_balance()))
