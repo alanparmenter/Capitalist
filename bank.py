4bff4e9e59cbf129f2d6cd4c85ed6f238eaef606
= <=>
2023-12-24 07:52:42 +0000

    Set initial bank balance to 10000 and make bank name mutable instead, perhaps for player name

--- bank.py
+++ bank.py
@@ -1,7 +1,7 @@
 class Bank:
-  def __init__(self, initial_deposit):
-    self.name = "Anonymous Bank"
-    self.cash = initial_deposit
+  def __init__(self, given_name):
+    self.name = given_name
+    self.cash = 10000
     self.interest_rate = 0
   def __repr__(self):
     return "This bank named {} has {} cash and an interest rate of {}".format(self.name, self.cash, self.interest_rate)
