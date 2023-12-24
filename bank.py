c6ec830e5d047f21b123cffcd354312838bc6739
= <=>
2023-12-24 07:22:31 +0000

    Separate Bank and Stock classes into their own files

---
+++ bank.py
@@ -0,0 +1,13 @@
+class Bank:
+  def __init__(self, initial_deposit):
+    self.name = "Anonymous Bank"
+    self.cash = initial_deposit
+    self.interest_rate = 0
+  def __repr__(self):
+    return "This bank named {} has {} cash and an interest rate of {}".format(self.name, self.cash, self.interest_rate)
+  def deposit(self, deposit_amount):
+    self.cash += deposit_amount
+  def withdraw(self, withdrawal_amount):
+    self.cash -= self.withdrawal_amount
+  def get_balance(self):
+    return self.cash
---
+++ stock.py
@@ -0,0 +1,15 @@
+class Stock:
+  def __init__(self, price):
+    self.price = price
+    self.holdings = 0.0
+    self.fees = 10
+  def __repr__(self):
+    return "This stock has {} price, {} holdings and {} fees".format(self.price, self.holdings, self.fees)
+  def buy(self, amount, bank):
+    self.holdings += (amount - self.fees) / self.price
+    band.withdraw(amount)
+  def sell(self, amount, bank):
+    self.holdings -= amount / self.price
+    bank.deposit(amount - self.fees)
+  def get_balance(self):
+    return self.holdings * self.price
