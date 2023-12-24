8602a7698e86b80fde46635893699fb80cf46041
= <=>
2023-12-24 07:10:06 +0000

    Add deposit, withdraw and get_balance methods to Bank class in capitalist.py

--- capitalist.py
+++ capitalist.py
@@ -20,4 +20,10 @@ class Bank:
     self.cash = initial_deposit
     self.interest_rate = 0
   def __repr__(self):
-    return "This bank named {} has {} cash and an interest rate of {}".format(self.name, self.cash, self.interest_rate)_
+    return "This bank named {} has {} cash and an interest rate of {}".format(self.name, self.cash, self.interest_rate)
+  def deposit(self, deposit_amount):
+    self.cash += deposit_amount
+  def withdraw(self.withdrawal_amount):
+    self.cash -= self.withdrawal_amount
+  def get_balance(self)
+    return self.cash
