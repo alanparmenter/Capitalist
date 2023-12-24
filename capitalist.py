04ecf7777ad00719f8f3a297e7e148b6c972d9e0
= <=>
2023-12-24 07:13:01 +0000

    Fix parameters typo in withdraw method

--- capitalist.py
+++ capitalist.py
@@ -23,7 +23,7 @@ class Bank:
     return "This bank named {} has {} cash and an interest rate of {}".format(self.name, self.cash, self.interest_rate)
   def deposit(self, deposit_amount):
     self.cash += deposit_amount
-  def withdraw(self.withdrawal_amount):
+  def withdraw(self, withdrawal_amount):
     self.cash -= self.withdrawal_amount
   def get_balance(self)
     return self.cash
